08c87f9d68787c83704cbf967f41c1e1a275cb17
BagasMuharom <bagashidayat45@gmail.com>
2017-11-23 15:44:58 +0700

    Menambah activity detail berita dan pengaturan, dan update MainActivity

--- app/src/main/java/com/smadia/jangka/Views/MainActivity.java
+++ app/src/main/java/com/smadia/jangka/Views/MainActivity.java
@@ -1,78 +1,60 @@
 package com.smadia.jangka.Views;
 
-import android.content.Context;
-import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
-import android.webkit.WebView;
-import android.widget.BaseAdapter;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.view.MenuItem;
 import android.widget.Toast;
 
-import com.smadia.jangka.Models.Online.Berita;
 import com.smadia.jangka.R;
-import com.smadia.jangka.Util.ConnectionChecker;
-
-import org.w3c.dom.Text;
-
-import java.util.ArrayList;
+import com.smadia.jangka.Views.Fragments.HomeFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    AppCompatActivity activity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.activity = this;
+        this.bottomNavigationView = (BottomNavigationView) this.findViewById(R.id.bottom_navigation);
+        this.bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
 
-        if(ConnectionChecker.isConnectedOnNetwork(this)) {
-            Berita berita = new Berita();
-            ListView listView = (ListView) this.findViewById(R.id.listView);
-            CustomAdapter customAdapter = new CustomAdapter(berita.all());
-            listView.setAdapter(customAdapter);
-        }
-        else
-            Toast.makeText(this, "Not Connected", Toast.LENGTH_LONG).show();
-    }
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment fragmentTerpilih = null;
+                        switch (item.getItemId()) {
+                            case R.id.profile:
+                                Toast.makeText(activity, "Sejarah", Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.home:
+                                fragmentTerpilih = new HomeFragment();
+                                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                                fragmentTransaction.replace(R.id.fragment_layout, fragmentTerpilih);
+                                fragmentTransaction.commit();
+                                break;
+                            case R.id.sejarah:
+                                Toast.makeText(activity, "Sejarah", Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.kategori:
+                                Toast.makeText(activity, "Kategori", Toast.LENGTH_SHORT).show();
+                                break;
+                        }
+                        return true;
+                    }
 
-    class CustomAdapter extends BaseAdapter {
+                });
 
-        ArrayList<Berita> daftarBerita;
-
-        public CustomAdapter(ArrayList<Berita> daftarBerita) {
-            this.daftarBerita = daftarBerita;
-        }
-
-        @Override
-        public int getCount() {
-            return this.daftarBerita.size();
-        }
-
-        @Override
-        public Object getItem(int i) {
-            return this.daftarBerita.get(i);
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return this.daftarBerita.get(i).getId();
-        }
-
-        @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
-            view = getLayoutInflater().inflate(R.layout.list_view, null);
-
-            TextView judul = (TextView) view.findViewById(R.id.judul);
-            TextView isi = (TextView) view.findViewById(R.id.isi);
-
-            judul.setText(this.daftarBerita.get(i).getJudul());
-            isi.setText(this.daftarBerita.get(i).getIsi());
-
-            return view;
-        }
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_layout, new HomeFragment());
+        fragmentTransaction.commit();
     }
 
 }
