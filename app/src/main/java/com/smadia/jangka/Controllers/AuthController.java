21bffbe6c3f74795e29f8bf0a95c3199fe4e97c2
ulif <ulifrosydah@gmail.com>
2017-12-07 17:18:16 +0700

    auth

--- app/src/main/java/com/smadia/jangka/Controllers/AuthController.java
+++ app/src/main/java/com/smadia/jangka/Controllers/AuthController.java
@@ -1,8 +1,24 @@
 package com.smadia.jangka.Controllers;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
+import com.smadia.jangka.Request.Request;
+import com.smadia.jangka.Util.App;
 import com.smadia.jangka.Views.MainActivity;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.sql.Blob;
 
 public class AuthController {
 
@@ -12,8 +28,108 @@ public class AuthController {
         this.activity = activity;
     }
 
-    public boolean login(String username, String password) {
+    public void login(String username, String password)
+    {
+        String url = App.generateUrl("login");
+        LoginListener listener = new LoginListener();
+        Request request = new Request(com.android.volley.Request.Method.POST, url, listener, listener);
+        request.addParams("username", username + "");
+        request.addParams("password", password + "");
+        RequestQueue queue = Volley.newRequestQueue(this.activity);
+        queue.add(request);
+    }
+
+    private class LoginListener implements Response.Listener<String>, Response.ErrorListener{
+
+        @Override
+        public void onResponse(String response) {
+                try {
+                    JSONObject jsonResponse = new JSONObject(response);
+                    boolean success = jsonResponse.getBoolean("success");
+
+                    if (success)
+                    {
+                        Toast.makeText(AuthController.this.activity, "Login Berhasil !", Toast.LENGTH_SHORT).show();
+
+                    }
+                    else
+                    {
+                        Toast.makeText(AuthController.this.activity, "Gagal Login !", Toast.LENGTH_SHORT).show();
+
+
+                    }
+
+                } catch (JSONException e) {
+
+                }
+        }
+
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Toast.makeText(AuthController.this.activity, "Terjadi kesalahan !", Toast.LENGTH_SHORT).show();
+        }
 
     }
+
+    public void register(String username, String email, String password, Blob avatar, boolean login)
+    {
+        String url = App.generateUrl("register");
+        RegisterListener listener = new RegisterListener();
+        Request request = new Request(com.android.volley.Request.Method.POST, url, listener, listener);
+        request.addParams("username", username + "");
+        request.addParams("email", password + "");
+        request.addParams("password", password + "");
+        request.addParams("password", avatar + "");
+        request.addParams("login", login + "");
+        RequestQueue queue = Volley.newRequestQueue(this.activity);
+        queue.add(request);
+    }
+
+    private class RegisterListener implements Response.Listener<String>, Response.ErrorListener{
+
+        @Override
+        public void onResponse(String response) {
+            try {
+                JSONObject jsonResponse = new JSONObject(response);
+                boolean success = jsonResponse.getBoolean("success");
+
+                if (success)
+                {
+                    Toast.makeText(AuthController.this.activity, "Anda Berhasil Terdaftar !", Toast.LENGTH_SHORT).show();
+
+                }
+                else
+                {
+                    Toast.makeText(AuthController.this.activity, "Gagal  !", Toast.LENGTH_SHORT).show();
+
+
+                }
+
+
+            } catch (JSONException e) {
+
+            }
+        }
+
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Toast.makeText(AuthController.this.activity, "Terjadi kesalahan !", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    public void logout(View view)
+    {
+        Toast.makeText(AuthController.this.activity, "Logged Out Successfully", Toast.LENGTH_LONG).show();
+
+        // Closing the current activity.
+        activity.finish();
+
+        // Redirect to Main Login activity after log out.
+        Intent intent = new Intent(AuthController.this.activity, MainActivity.class);
+
+        activity.startActivity(intent);
+    }
+
 
 }
--- app/src/main/java/com/smadia/jangka/RegisterActivity.java
+++ app/src/main/java/com/smadia/jangka/RegisterActivity.java
@@ -2,12 +2,35 @@ package com.smadia.jangka;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnRegister;
+    EditText editUname, editEmail, editPswrd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        btnRegister = (Button) findViewById(R.id.btnRegis);
+        editUname = (EditText) findViewById(R.id.editUsname);
+        editEmail = (EditText) findViewById(R.id.editMail);
+        editPswrd = (EditText) findViewById(R.id.editPsw);
+
+        btnRegister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.btnRegis:
+
+                break;
+        }
     }
 }
